012e4a7504f65c146f53e93a64a51f720ebf2aa5
Abdullahi <muxsinxareed6@gmail.com>
2023-09-11 12:00:51 +0200

    Substantiv

--- app.java
+++ app.java
@@ -4,7 +4,7 @@ public class app {
 public static void main(String[] args) {
 Scanner newScanner = new Scanner(System.in);
 
-
+/* 
 System.out.println("Divisions Program: ");
 
 System.out.print("Ange första tal ");
@@ -16,6 +16,18 @@ int secondNum = newScanner.nextInt();
 
 double division = (double) firstNum / secondNum;
 System.out.println("Kvoten är " + division);
+*/
+
+System.out.print("Ange ett substantiv: ");
+String noun = newScanner.nextLine();
+
+System.out.print("Ange substantivets pluralhändelse: ");
+String pluralForm = newScanner.nextLine();
+
+System.out.println("En " + noun + ", " + "flera " + noun + pluralForm);
+
+
+
 
 
 }
